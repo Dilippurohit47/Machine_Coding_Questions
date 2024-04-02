3b406c48642010bd4221ae68fabaa78c1c12b92c
Dilippurohit47 <dilippurohit204@gmail.com>
2024-04-02 15:42:43 +0530

    selectable_grid

---
+++ scalabe grid/app/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import SelectableGrid from './components/SelectableGrid'
+function App() {
+
+  return <div className='main'> 
+    <h1>Selectable Grid</h1>
+    <SelectableGrid rows={10} cols={10} />
+  </div>
+}
+
+export default App
